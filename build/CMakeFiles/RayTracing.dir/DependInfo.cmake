b5dc156a9c0f127f91478a02d654444e594613ac
yindec <yindec17@outlook.com>
2023-08-30 16:54:51 +0800

    Add build

---
+++ build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/WorkSpace/Git/code/BVH.cpp" "CMakeFiles/RayTracing.dir/BVH.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/BVH.cpp.obj.d"
+  "D:/WorkSpace/Git/code/Renderer.cpp" "CMakeFiles/RayTracing.dir/Renderer.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Renderer.cpp.obj.d"
+  "D:/WorkSpace/Git/code/Scene.cpp" "CMakeFiles/RayTracing.dir/Scene.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Scene.cpp.obj.d"
+  "D:/WorkSpace/Git/code/Vector.cpp" "CMakeFiles/RayTracing.dir/Vector.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Vector.cpp.obj.d"
+  "D:/WorkSpace/Git/code/main.cpp" "CMakeFiles/RayTracing.dir/main.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
